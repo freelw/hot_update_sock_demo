56520b1da9931c9a358995c69ed84b87c3bdee21
wangli <liwang112358@gmail.com>
2020-12-01 19:59:56 +0800

    update

--- update.cpp
+++ update.cpp
@@ -7,7 +7,7 @@ int readed(int fd, char* read_buffer, int length)
     while (recv_len < length) {
         int read_size = read(fd, read_buffer + recv_len, length - recv_len);
         if (read_size <= 0) {
-            break;
+            return -1;
         }
         recv_len += read_size;
     }
@@ -18,7 +18,10 @@ int main(){
     printf("program updated.\n");
     while(1) {
         char req[5] = {0};
-        readed(4, req, 5);
+        if (readed(4, req, 5) < 0) {
+            printf("read error.\n");
+            break;
+        }
         printf("updated recv: %s\n", req);
     }
     return 0;
